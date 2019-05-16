206e76dc355890a5a9f1b0e34873dbee0b1c4049
ephammer <kidon.613@gmail.com>
2019-05-16 13:06:13 +0300

    androidx migration && initial actif=vity setup

---
+++ app/src/main/java/com/thinkhodl/lifetag/EditInfoActivity.java
@@ -0,0 +1,13 @@
+package com.thinkhodl.lifetag;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_info);
+    }
+}
--- app/src/main/java/com/thinkhodl/lifetag/MainActivity.java
+++ app/src/main/java/com/thinkhodl/lifetag/MainActivity.java
@@ -1,13 +1,44 @@
 package com.thinkhodl.lifetag;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.nfc_view)
+    ConstraintLayout mNFCLayout;
+
+    @BindView(R.id.nfc_antena_imageview)
+    ImageView mNFCAntenaImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        animNFC();
     }
+
+    private void animNFC(){
+
+        ObjectAnimator fadeOut = ObjectAnimator.ofFloat(mNFCAntenaImageView, "alpha",  1f, 0);
+        fadeOut.setDuration(2000);
+        fadeOut.setRepeatMode(ValueAnimator.RESTART);
+        fadeOut.setRepeatCount(Animation.INFINITE);
+        fadeOut.start();
+
+    }
+
 }
