3fbcdf336612787bd1a6dc56e93b1e5e67fe98d9
ephammer <kidon.613@gmail.com>
2019-05-16 18:01:01 +0300

    send sms button

--- app/src/main/java/com/thinkhodl/lifetag/ProfileInfoActivity.java
+++ app/src/main/java/com/thinkhodl/lifetag/ProfileInfoActivity.java
@@ -3,19 +3,48 @@ package com.thinkhodl.lifetag;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.security.Permission;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ProfileInfoActivity extends AppCompatActivity {
+
+    @BindView(R.id.contact_emergency_button)
+    Button mEmergencyButton;
+
+    @BindView(R.id.emergency_contact_number_textView)
+    TextInputEditText mEmergencyContactNumberTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_info);
+
+        ButterKnife.bind(this);
+
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         myToolbar.setTitle("");
         setSupportActionBar(myToolbar);
@@ -27,6 +56,13 @@ public class ProfileInfoActivity extends AppCompatActivity {
         if (ab != null) {
             ab.setDisplayHomeAsUpEnabled(true);
         }
+
+        mEmergencyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSMSMessage();
+            }
+        });
 
     }
 
@@ -51,5 +87,27 @@ public class ProfileInfoActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    protected void sendSMSMessage() {
+
+        Dexter.withActivity(ProfileInfoActivity.this)
+                .withPermission(Manifest.permission.SEND_SMS)
+                .withListener(new PermissionListener() {
+                    @Override public void onPermissionGranted(PermissionGrantedResponse response) {
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(
+                                mEmergencyContactNumberTextView.getText().toString(),
+                                null,
+                                "Hey an emergency occured",
+                                null,
+                                null);
+                    }
+                    @Override public void onPermissionDenied(PermissionDeniedResponse response) {/* ... */}
+                    @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {/* ... */}
+                }).check();
+
+
     }
 }
